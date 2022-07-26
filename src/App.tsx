bfcd058a400b08cbb30ef65f238b2283883fda4c
yonatan104 <yonatangeller@gmail.com>
2022-07-26 21:27:52 +0300

    first commit set up react.ts adding files routing and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom"
+import { SigninSignupPage } from "./pages/signin-signup-page"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<SigninSignupPage />} />
+        <Route path="/signin" element={<SigninSignupPage />} />
+        <Route path="/login" element={<SigninSignupPage />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/pages/signin-signup-page.tsx
@@ -0,0 +1,13 @@
+export const SigninSignupPage = () => {
+  return (
+    <div className="sign-container">
+      <div className="sign-modal">
+        <form action="">
+          <input type="text" placeholder="Enter full name" />
+          <input type="text" placeholder="Enter username" />
+          <input type="text" placeholder="Enter password" />
+        </form>
+      </div>
+    </div>
+  )
+}
