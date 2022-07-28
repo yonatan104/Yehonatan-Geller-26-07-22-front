4d5374f118200c4ac994c003a9b232bd405bbe38
yonatan104 <yonatangeller@gmail.com>
2022-07-28 18:40:38 +0300

    chat page looks better.

--- src/cmps/message-preview.tsx
+++ src/cmps/message-preview.tsx
@@ -4,5 +4,10 @@ type MessageProps = {
   message: Message;
 }
 export const MessagePreview = ({ message }: MessageProps) => {
-return <div className="message-container">{message.text}</div>;
+return <div className="message-container">
+<div className="message-content {{messageContent}}">
+        <div className="text">{message.text}</div>
+        {/* <div className="time">{message.createAt| date:'shortTime'}</div> */}
+    </div>
+</div>;
 }
--- src/pages/chat-page.tsx
+++ src/pages/chat-page.tsx
@@ -11,58 +11,67 @@ import { userService } from "../services/user.service";
 import { utilService } from "../services/util.service";
 
 export const Chat = () => {
-    const [chatRoom,setChatRoom] = useState({}as ChatRoom)
-    const [user, setUser] = useState({} as User);
-    const params = useParams();
+  const [chatRoom, setChatRoom] = useState({} as ChatRoom);
+  const [user, setUser] = useState({} as User);
+  const params = useParams();
   useEffect(() => {
     loadChatRoom();
-  },[]);
-  useEffect(() => {
-    socketService.on('new-message', onSetChatRoom);
   }, []);
-  
-    const onSetChatRoom = async (chatRoom: SetStateAction<ChatRoom>) => {
-      setChatRoom( chatRoom);
-    };
+  useEffect(() => {
+    socketService.on("new-message", onSetChatRoom);
+  }, []);
+
+  const onSetChatRoom = async (chatRoom: SetStateAction<ChatRoom>) => {
+    setChatRoom(chatRoom);
+  };
   const loadChatRoom = async () => {
     try {
-      const {friendId, chatRoomId} = params
-        if (!friendId || !chatRoomId)return console.error("There is not id in params");
-        const user = await userService.getById(friendId);
-        console.log("🚀 ~ file: chat-page.tsx ~ line 19 ~ loadChatRoom ~ user", user)
-        const chatRoomFromData = await chatRoomService.getById(chatRoomId);
-        console.log("🚀 ~ file: chat-page.tsx ~ line 16 ~ loadChatRoom ~ chatRoomFromData", chatRoomFromData)
-        setChatRoom(chatRoomFromData);
-        setUser(user);
+      const { friendId, chatRoomId } = params;
+      if (!friendId || !chatRoomId)
+        return console.error("There is not id in params");
+      const user = await userService.getById(friendId);
+      console.log(
+        "🚀 ~ file: chat-page.tsx ~ line 19 ~ loadChatRoom ~ user",
+        user
+      );
+      const chatRoomFromData = await chatRoomService.getById(chatRoomId);
+      console.log(
+        "🚀 ~ file: chat-page.tsx ~ line 16 ~ loadChatRoom ~ chatRoomFromData",
+        chatRoomFromData
+      );
+      setChatRoom(chatRoomFromData);
+      setUser(user);
     } catch (error) {
-        console.error('Can not load chat room', error);
+      console.error("Can not load chat room", error);
     }
   };
-  const onSendMsg = async(text:string) =>{
-    const loggedUser = userService.getLoggedinUser()
-    if (text === '') return 
+  const onSendMsg = async (text: string) => {
+    const loggedUser = userService.getLoggedinUser();
+    if (text === "") return;
     const message = {
       _id: utilService.makeId(),
-      createAt:Date.now(),
-      text:text,
-      fromUser:{
-        username:loggedUser.username,
-        imgUrl:loggedUser.imgUrl,
-        userId:loggedUser._id,
-      } 
+      createAt: Date.now(),
+      text: text,
+      fromUser: {
+        username: loggedUser.username,
+        imgUrl: loggedUser.imgUrl,
+        userId: loggedUser._id,
+      },
     } as Message;
-    const chatRoomTosend = chatRoom
+    const chatRoomTosend = chatRoom;
     chatRoomTosend.messages.push({ ...message });
     socketService.emit("send-message", chatRoomTosend);
-     const savedChatRoom= await chatRoomService.save(chatRoomTosend);
-     setChatRoom(savedChatRoom)
-  }
-  if (!chatRoom||!chatRoom.messages) return <div>loading...</div>;
+    const savedChatRoom = await chatRoomService.save(chatRoomTosend);
+    setChatRoom(savedChatRoom);
+  };
+  if (!chatRoom || !chatRoom.messages) return <div>loading...</div>;
   return (
     <div className="chat-page-container">
-      <ChatHeader user={user}/>
-      <MessageList messages={chatRoom.messages} />
-      <ChatFooter sendMsg={onSendMsg} />
+      <div className="chat-page">
+        <ChatHeader user={user} />
+        <MessageList messages={chatRoom.messages} />
+        <ChatFooter sendMsg={onSendMsg} />
+      </div>
     </div>
   );
 };
